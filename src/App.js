c49463eabe0722f78408f81df88b4abcf266280f
shahnozaqarshiboyeva <quyoshcha2005@gmail.com>
2023-04-15 14:25:33 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {CheckboxGroup, Checkbox} from '@adobe/react-spectrum'
+import { useState ,useEffect } from 'react'
+import axios from 'axios'
+export default function App() {
+  const [users,setUsers]=useState([])
+  useEffect(()=>{
+   axios.get('https://jsonplaceholder.typicode.com/todos').then(Response=>{
+    setUsers(Response.data)
+   })
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className="row">
+        <div className="col-md-8 offset-2 mt-3">
+          <table className='table table-bordered table-stripped table-hover'>
+            <thead>
+              <tr>
+                <th>T/R</th>
+                <th>userId</th>
+                <th>Title</th>
+                <th>Completed</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                users.map((item,index)=>{
+                  return <tr key={index}>
+                    <td>{index+1}</td>
+                    <td>{item.userId}</td>
+                    <td>{item.title}</td>
+                    <td><input value={item.completed} type="checkbox"/></td>
+                  </tr>
+                })
+              }
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/Components/First/First.js
@@ -0,0 +1,25 @@
+// import React from 'react'
+// import { useContext } from 'react'
+// import { myContext } from '../../App'
+// export default function First() {
+//     const value =useContext (myContext)
+//   return (
+//     <div>
+//         <h1>My name is {this.myHandler}</h1>
+//     </div>
+//   )
+// }
+// import React from 'react'
+// import { useState ,createContext } from 'react'
+// import First from './Components/First/First'
+// export const myContext=createContext()
+// export default function App() {
+//   myHandler=()=>{
+//     console.log('Shahnoza');
+//   }
+//   return (
+//     <myContext.Provider value={this.myHandler}>
+//       <First/>
+//     </myContext.Provider>
+//   )
+// }
